f97c6f77d9d38dc0d1fe9d5d24e30cecfae12c3b
Riley Mathews <rileymathews80@gmail.com>
2018-10-02 21:03:57 -0500

    created technology model

---
+++ website/models/technology.py
@@ -0,0 +1,6 @@
+from django.db import models
+
+class Technology(models.Model):
+    name = models.CharField(max_length=255)
+    description = models.CharField(max_length=255)
+    picture_url = models.CharField(max_length=1000)

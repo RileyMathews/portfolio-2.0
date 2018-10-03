6be63a9b94bfa7cb457764b2cad1c47607bbf6c8
Riley Mathews <rileymathews80@gmail.com>
2018-10-02 21:07:36 -0500

    added str method to technology

--- website/models/technology.py
+++ website/models/technology.py
@@ -4,3 +4,6 @@ class Technology(models.Model):
     name = models.CharField(max_length=255)
     description = models.CharField(max_length=255)
     picture_url = models.CharField(max_length=1000)
+
+    def __str__(self):
+        return self.name

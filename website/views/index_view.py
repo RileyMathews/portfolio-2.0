9c33e01c1dd39977ba78073aab83a6dd313062c9
Riley Mathews <rileymathews80@gmail.com>
2018-10-03 20:20:45 -0500

    added extra styling

--- website/views/index_view.py
+++ website/views/index_view.py
@@ -1,5 +1,5 @@
 from django.views.generic import TemplateView
-from website.models import Technology, Project
+from website.models import Technology, Project, Bio
 from django.shortcuts import render
 
 
@@ -15,7 +15,8 @@ def index_view(request):
     print("loading index")
     technologies = Technology.objects.all()
     projects = Project.objects.all()
+    bio = Bio.objects.get(pk=1)
 
-    data = {"technologies": technologies, "projects": projects}
+    data = {"technologies": technologies, "projects": projects, "bio": bio}
     return render(request, "website/index.html", {"data": data})
 

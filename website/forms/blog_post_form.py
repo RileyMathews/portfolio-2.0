fb51fab310323848f290deece25cac10079aaeb6
Riley Mathews <rileymathews80@gmail.com>
2018-10-23 15:55:11 +0200

    cleaned up blog

--- website/forms/blog_post_form.py
+++ website/forms/blog_post_form.py
@@ -5,3 +5,9 @@ class BlogPostForm(forms.ModelForm):
     class Meta:
         model = BlogPost
         fields = ('title', 'subtitle', 'text', 'categories',)
+        widgets = {
+            'title': forms.TextInput(attrs={'class': 'text-input'}),
+            'subtitle': forms.TextInput(attrs={'class': 'text-input'}),
+            'text': forms.Textarea(attrs={'class': 'text-area'}),
+            'categories': forms.SelectMultiple(attrs={'class': 'input-select'}),
+        }
